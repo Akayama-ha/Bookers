7bc6ae13b3423eb49847ca28edb332e5e4792461
Akayama-ha <127019742+>
2023-03-21 08:27:39 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Posted successfully"
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Posted successfully"
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -37,8 +37,10 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.destroy(book_params)
+      flash[:notice] = "It has been deleted"
+      redirect_to '/books'
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :books
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  resources :books do
+   collection do
+     get 'top'
+   end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.hendend
 end
